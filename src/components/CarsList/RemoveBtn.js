c65056ba26ff17720c34b6c90f0d58750fa19912
Néstor Argüello <nestor.arguello@gmail.com>
2019-05-15 21:26:48 -0400

    add confirmation modal on remove car

--- src/components/CarsList/RemoveBtn.js
+++ src/components/CarsList/RemoveBtn.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import { useStateValue } from '../../globalState';
 import { removeCar } from '../../actions';
 
 const RemoveBtn = ({ record, ...props }) => {
   const dispatch = useStateValue()[1];
+  const confirm = Modal.confirm;
+  const showConfirmRemove = () => {
+    confirm({
+      title: '¿Deseas eliminar este vehículo?',
+      content: 'Se eliminará permanentemente de tu registro',
+      okText: 'Eliminar',
+      okType: 'danger',
+      okButtonProps: {
+        disabled: false
+      },
+      autoFocusButton: 'cancel',
+      maskClosable: true,
+      cancelText: 'Cancelar',
+      onOk() {
+        dispatch(removeCar(record.key));
+      },
+      onCancel() {
+        return;
+      }
+    });
+  };
   const handleClick = event => {
     event.preventDefault();
-    dispatch(removeCar(record.key));
+    showConfirmRemove();
   };
 
   return (
